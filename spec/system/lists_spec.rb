8bcbfef2ea62ddc7fa1a5cee40e8eb36d7bc60fd
EC2 Default User <ec2-user@ip-172-31-85-9.ec2.internal>
2021-05-02 12:22:41 +0000

    finish rspec

--- spec/system/lists_spec.rb
+++ spec/system/lists_spec.rb
@@ -2,9 +2,7 @@
 
 require 'rails_helper'
 describe '投稿のテスト' do
-  
   let!(:list) { create(:list, title:'hoge',body:'body') }
-  
   describe 'トップ画面(top_path)のテスト' do
     before do
       visit top_path
@@ -41,31 +39,64 @@ describe '投稿のテスト' do
     end
   end
 
-  describe：一覧画面のテスト
-    before：一覧画面への遷移
-    context ：一覧の表示とリンクの確認
-      it：一覧表示画面に投稿されたもの表示されているか
-        テストコード
+  describe "投稿一覧のテスト" do
+    before do
+      visit todolists_path
+    end
+    context '表示の確認' do
+      it '投稿されたものが表示されているか' do
+        expect(page).to have_content list.title
+        expect(page).to have_link list.title
+      end
+    end
+  end
 
-  describe：詳細画面のテスト
-    before：詳細画面への遷移
-    context：表示のテスト
-      it：削除リンクが存在しているか
-        テストコード
-      it：編集リンクが存在しているか
-    context：リンクの遷移先の確認
-      it：編集の遷移先は編集画面か
-        テストコード
-    context：list削除のテスト
-      it：listの削除
-        テストコード 
-        
-  describe：編集画面のテスト
-    before：編集画面への遷移
-    context：表示の確認
-      it：編集前のタイトルと本文がフォームに表示(セット)されている
-        テストコード
-      it：保存ボタンが表示される
-    context：更新処理に関するテスト
-      it：更新後のリダイレクト先は正しいか
-        テストコード
+  describe "詳細画面のテスト" do
+    before do
+      visit todolist_path(list)
+    end
+    context '表示の確認' do
+      it '削除リンクが存在しているか' do
+        expect(page).to have_link '削除'
+      end
+      it '編集リンクが存在しているか' do
+        expect(page).to have_link '編集'
+      end
+    end
+    context 'リンクの遷移先の確認' do
+      it '編集の遷移先は編集画面か' do
+        edit_link = find_all('a')[0]
+        edit_link.click
+        expect(current_path).to eq('/todolists/' + list.id.to_s + '/edit')
+      end
+    end
+    context 'list削除のテスト' do
+      it 'listの削除' do
+        expect{ list.destroy }.to change{ List.count }.by(-1)
+      end
+    end
+  end
+
+  describe '編集画面のテスト' do
+    before do
+      visit edit_todolist_path(list)
+    end
+    context '表示の確認' do
+      it '編集前のタイトルと本文がフォームに表示(セット)されている' do
+        expect(page).to have_field 'list[title]', with: list.title
+        expect(page).to have_field 'list[body]', with: list.body
+      end
+      it '保存ボタンが表示される' do
+        expect(page).to have_button '保存'
+      end
+    end
+    context '更新処理に関するテスト' do
+      it '更新後のリダイレクト先は正しいか' do
+        fill_in 'list[title]', with: Faker::Lorem.characters(number:5)
+        fill_in 'list[body]', with: Faker::Lorem.characters(number:20)
+        click_button '保存'
+        expect(page).to have_current_path todolist_path(list)
+      end
+    end
+  end
+end
